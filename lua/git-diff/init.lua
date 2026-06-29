da53c36d9a3486bb26f4259ec38c07b1c31f02af
Andrew Venson <vensoneandrew@gmail.com>
2026-06-29 17:10:19 -0500

    local-diff -> git-diff

--- lua/git-diff/init.lua
+++ lua/git-diff/init.lua
@@ -1,15 +1,15 @@
 local M = {}
 
 function M.setup(opts)
-  opts = opts or {}
+	opts = opts or {}
 
-  vim.keymap.set('n', '<leader>gd', function()
-    require('local-diff.git').show()
-  end, { desc = '[G]it local [D]iff (vs base branch)' })
+	vim.keymap.set("n", "<leader>gd", function()
+		require("gitt-diff.git").show()
+	end, { desc = "[G]it local [D]iff (vs base branch)" })
 
-  vim.keymap.set('n', '<leader>gD', function()
-    require('local-diff.git').show_with_prompt()
-  end, { desc = '[G]it local [D]iff vs custom branch/commit' })
+	vim.keymap.set("n", "<leader>gD", function()
+		require("git-diff.git").show_with_prompt()
+	end, { desc = "[G]it local [D]iff vs custom branch/commit" })
 end
 
 return M
